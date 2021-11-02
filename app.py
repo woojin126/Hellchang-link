416e63efa4bc28e28a8af5bbf9b3f7299447b03b
dogmaster <cwd3469@naver.com>
2021-11-02 14:02:36 +0900

    app renewal

--- app.py
+++ app.py
@@ -1,40 +1,31 @@
 from flask import Flask, render_template, jsonify, request
-
 app = Flask(__name__)
 
 from pymongo import MongoClient
-
-# client = MongoClient('mongodb://test:test@localhost', 27017)
 client = MongoClient('localhost', 27017)
-db = client.dbsparta
+db = client.plus_week1
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
-# @app.route('/api/member', methods=['GET'])
-# def mainPage():
-#     return render_template('index.html')
+@app.route('/diary', methods=['GET'])
+def show_diary():
+    sample_receive = request.args.get('sample_give')
+    print(sample_receive)
+    return jsonify({'msg': 'GET 연결 완료!'})
 
-
-@app.route('/api/member')
-def mainPage():
-    if request.method == 'GET':
-        return render_template('index.html')
-    else:
-        member_Id = request.form['me_id']
-        member_Pw = request.form['me_pw']
-        member_Email = request.form['me_email']
-        member_Name = request.form['me_name']
-
-        doc = {
-            'me_id': member_Id,
-            'me_pw': member_Pw,
-            'me_email': member_Email,
-            'member_name': member_Name
-        }
-
-        db.hellchangRegister.insert_one(doc)
-
-        return jsonify({'msg': '회원가입이 되었습니다'})
-
+@app.route('/diary', methods=['POST'])
+def save_diary():
+   title_receive = request.form['title_give']
+   content_receive = request.form['content_give']
+   doc = {
+       'title': title_receive,
+       'content': content_receive,
+   }
+   db.diary.insert_one(doc)
+   return jsonify({'msg': 'POST 요청 완료!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
