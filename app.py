457b2df276bb456a2a8e9831ccf5a0b613ad34c8
ghm8614 <ghm8614@gmail.com>
2021-11-02 18:04:48 +0900

    api url update

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def mainPage():
 
 
 # API
-@app.route('/register', methods=['POST'])
+@app.route('/api/register', methods=['POST'])
 def register():
     member_Id = request.form['me_id']
     member_Pw = request.form['me_pw']
@@ -50,8 +50,8 @@ def register():
 
 # 로그인 기능
 SECRET_KEY = 'SPARTA'
-@app.route('/login', methods=['POST'])
-def login2():
+@app.route('/api/login', methods=['POST'])
+def login():
     id_receive = request.form['me_id']
     pw_receive = request.form['me_pw']
 
