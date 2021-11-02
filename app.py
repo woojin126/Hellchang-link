b4c9ac6eaa606ef050707c7eb2ef44eab0f49855
woojin126 <woojin126@naver.com>
2021-11-02 15:06:02 +0900

    LOGIN JWT Issued

--- app.py
+++ app.py
@@ -1,31 +1,58 @@
 from flask import Flask, render_template, jsonify, request
+from flask_jwt_extended import *
+
 app = Flask(__name__)
 
 from pymongo import MongoClient
-client = MongoClient('localhost', 27017)
-db = client.plus_week1
 
-@app.route('/')
-def home():
+# client = MongoClient('mongodb://test:test@localhost', 27017)
+client = MongoClient('localhost', 27017)
+db = client.dbsparta
+
+
+@app.route('/api/member', methods=['GET'])
+def mainPage():
     return render_template('index.html')
 
 
-@app.route('/diary', methods=['GET'])
-def show_diary():
-    sample_receive = request.args.get('sample_give')
-    print(sample_receive)
-    return jsonify({'msg': 'GET 연결 완료!'})
+@app.route('/api/sign_up', methods=['POST'])
+def register():
+    member_Id = request.form['me_id']
+    member_Pw = request.form['me_pw']
+    member_Email = request.form['me_email']
+    member_Name = request.form['me_name']
 
-@app.route('/diary', methods=['POST'])
-def save_diary():
-   title_receive = request.form['title_give']
-   content_receive = request.form['content_give']
-   doc = {
-       'title': title_receive,
-       'content': content_receive,
-   }
-   db.diary.insert_one(doc)
-   return jsonify({'msg': 'POST 요청 완료!'})
+    doc = {
+        'me_id': member_Id,
+        'me_pw': member_Pw,
+        'me_email': member_Email,
+        'member_name': member_Name
+    }
+
+    db.hellchangRegister.insert_one(doc)
+
+    return jsonify({'msg': '회원가입이 되었습니다'})
+
+
+@app.route('/api/login', methods=['POST'])
+def login():
+    me_id = request.form['me_id']
+    me_pw = request.form['me_pw']
+
+    member = db.hellchangRegister.find_one({'me_id': me_id}, {'me_pw': me_pw})
+    if member is None:
+        return jsonify({'login': False})
+
+    access_token = create_access_token(identity=me_id, expires_delta=False)
+    refresh_token = create_refresh_token(identity=me_id)
+
+    resp = jsonify({'login': True})
+
+    set_access_cookies(access_token)
+    set_refresh_cookies(refresh_token)
+
+    return resp,200
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
